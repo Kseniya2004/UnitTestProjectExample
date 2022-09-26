c62e3dd9b9ad318f3788b75b87480146fba8d3b6
Немтырёва Ксения <k.nemtyryova@gmail.com>
2022-09-26 12:25:25 +0300

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectExample")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5bdeb26-a244-43cb-aae0-b936d0496554")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestSquareSolve.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryExample;
+
+namespace UnitTestProjectExample
+{
+    [TestClass]
+    public class UnitTestSquareSolve
+    {
+        [TestMethod]
+        public void TestMethod_x1_1_minus5_6_returned3()
+        {//x^2 - 5x + 6 = 0
+            //d=1 x1=3
+            int a = 1;
+            int b = -5;
+            int c = 6;
+            string xTest = "3";
+            // вызов метода
+            ClassSquareSolve x = new ClassSquareSolve();            
+            Assert.AreEqual(xTest, x.x1(a, b, c));//сравнение на равенство ожидаемого и фактического значений
+        }
+        [TestMethod]
+        public void TestMethod_x2_1_minus5_6_returned2()
+        {
+            //x^2 - 5x + 6 = 0
+            //d=1 x1=3
+            int a = 1;
+            int b = -5;
+            int c = 6;
+            string xTest = "2";
+            // вызов метода
+            ClassSquareSolve x = new ClassSquareSolve();
+            Assert.AreEqual(xTest, x.x2(a, b, c));//сравнение на равенство ожидаемого и фактического значений
+        }
+        [TestMethod]
+        public void TestMethod_x1_3_1_3_returnedNo()
+        {
+            int a = 3;
+            int b = 1;
+            int c = 3;
+            string xTest = "Действительных корней нет";
+            // вызов метода
+            ClassSquareSolve x = new ClassSquareSolve();
+            Assert.AreEqual(xTest, x.x1(a, b, c));//сравнение на равенство ожидаемого и фактического значений
+        }
+        [TestMethod]
+        public void TestMethod_x2_3_1_3_returnedNo()
+        {
+            int a = 3;
+            int b = 1;
+            int c = 3;
+            string xTest = "Действительных корней нет";
+            // вызов метода
+            ClassSquareSolve x = new ClassSquareSolve();
+            Assert.AreEqual(xTest, x.x2(a, b, c));//сравнение на равенство ожидаемого и фактического значений
+        }
+        [TestMethod]
+        public void TestMethod_x1_0_1_3_returned3()
+        {
+            int a = 0;
+            int b = 1;
+            int c = 3;
+            string xTest = "-3";
+            // вызов метода
+            ClassSquareSolve x = new ClassSquareSolve();
+            Assert.AreEqual(xTest, x.x1(a, b, c));//сравнение на равенство ожидаемого и фактического значений
+        }
+        [TestMethod]
+        public void TestMethod_x2_0_1_3_returned3()
+        {
+            int a = 0;
+            int b = 1;
+            int c = 3;
+            string xTest = "-3";
+            // вызов метода
+            ClassSquareSolve x = new ClassSquareSolve();
+            Assert.AreEqual(xTest, x.x2(a, b, c));//сравнение на равенство ожидаемого и фактического значений
+        }
+        [TestMethod]
+        public void TestMethod_x1_0_0_0_returned0()
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            string xTest = "0";
+            // вызов метода
+            ClassSquareSolve x = new ClassSquareSolve();
+            Assert.AreEqual(xTest, x.x1(a, b, c));//сравнение на равенство ожидаемого и фактического значений
+        }
+        [TestMethod]
+        public void TestMethod_x2_0_0_0_returned3()
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            string xTest = "Действительных корней нет";
+            // вызов метода
+            ClassSquareSolve x = new ClassSquareSolve();
+            Assert.AreEqual(xTest, x.x2(a, b, c));//сравнение на равенство ожидаемого и фактического значений
+        }
+    }
+}
